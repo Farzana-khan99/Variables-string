cdfe3109000f112b68897918d11302c25430390d
Farzana <id.farzankhan@gmail.com>
2023-09-30 22:50:42 +0500

    assignment variables and string

---
+++ js.js
@@ -0,0 +1,32 @@
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+let age = 15;
+alert("I am"  + " " + age +" " + " years old")
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+let vistedSite = 14;
+
+alert("you have visited this site " + " " + vistedSite + " times")
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+let birthYear = 1990;
+console.log("My birth year is " + birthYear + "\n" + "Data type of my declear variable is nymber" )
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+let visitorName ="Jhone Doe" , productsTitle ="T-shirt(s)" , Quantity ="XYZ Clothing store";
+
+console.log(visitorName + " " + " ordered" + " " +productsTitle + " " + "on" +" "+ Quantity)
